f3e38dcee9a5e75336460f805d1205bf62c832a6
mujibmujiii <mujibmukka@gmail.com>
2026-01-28 21:38:45 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+#even_odd.py
+# This program checks if a number is even or odd
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ string_chars.py
@@ -0,0 +1,7 @@
+# string_chars.py
+# This program prints the first and last character of a string
+
+text = input("Enter a string: ")
+
+print("First character:", text[0])
+print("Last character:", text[-1])
